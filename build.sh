d3f094c2a9abc17d4777735fb6647b3e70c1b855
rsundahl <roy@polyverse.io>
2017-09-29 10:24:05 -0700

    Allowing an optional parameter (for -s) to pv build docker.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-pv build -f Dockerfile.alpine -n tcp_echo_server.alpine docker
-pv build -f Dockerfile.centos -n tcp_echo_server.centos docker
-pv build -f Dockerfile.ubuntu -n tcp_echo_server.ubuntu docker
+pv build -f Dockerfile.alpine -n tcp_echo_server.alpine $1 docker
+pv build -f Dockerfile.centos -n tcp_echo_server.centos $1 docker
+pv build -f Dockerfile.ubuntu -n tcp_echo_server.ubuntu $1 docker
