4d78e7daeb7b1ea4c898bbea3aed51a1a6f4857d
rsundahl <roy@polyverse.io>
2017-10-04 11:23:53 -0700

    Added support for xenial and zesty.

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 pv build -f Dockerfile.alpine -n tcp_echo_server.alpine $1 docker
 pv build -f Dockerfile.centos -n tcp_echo_server.centos $1 docker
-pv build -f Dockerfile.ubuntu -n tcp_echo_server.ubuntu $1 docker
+pv build -f Dockerfile.trusty -n tcp_echo_server.trusty $1 docker
+pv build -f Dockerfile.xenial -n tcp_echo_server.xenial $1 docker
+pv build -f Dockerfile.zesty  -n tcp_echo_server.zesty  $1 docker
