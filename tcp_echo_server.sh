3b285c885d40bd7afe50e14acdbe67f36753b54d
rsundahl <roy@polyverse.io>
2017-10-11 09:45:24 -0700

    Default to xenial w/o dev tools.

--- tcp_echo_server.sh
+++ tcp_echo_server.sh
@@ -7,7 +7,7 @@ target=xenial
 #target=zesty
 
 # Use this command to run
-#docker run -it --rm -p 8080:8080 --name echo polyverse/tcp_echo_server.$target
+docker run -it --rm -p 8080:8080 --name echo polyverse/tcp_echo_server.$target
 
 # Use this command to debug (and run the server by hand internally, using gdb)
-docker run --privileged -it --rm -p 8080:8080 --name echo --entrypoint /bin/bash polyverse/tcp_echo_server.$target
+# docker run --privileged -it --rm -p 8080:8080 --name echo --entrypoint /bin/bash polyverse/tcp_echo_server.$target
