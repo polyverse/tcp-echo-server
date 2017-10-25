4b4f2a64c683ee2a9d5c4e0173b60a2f385d9a7d
rsundahl <roy@polyverse.io>
2017-10-25 12:56:05 -0700

    Require an OS parameter to tcp_echo_server.sh.

--- tcp_echo_server.sh
+++ tcp_echo_server.sh
@@ -1,13 +1,23 @@
 #! /bin/bash
 
-#target=alpine
-#target=centos
-#target=trusty
-target=xenial
-#target=zesty
+SHORT_DESCRIPTION="Launch the vulnerable tcp_echo_server container for the given OS."
+
+USAGE="$(cat <<EOF
+
+Usage: sh tcp_echo_server.sh ${0##*/} [alpine|centos|trusty|xenial|zesty]
+
+$SHORT_DESCRIPTION
+
+EOF
+)"
+
+if [ $# -ne 1 ] || ! [[ "$1" =~ ^(alpine|centos|trusty|xenial|zesty)$ ]]; then
+  printf "$USAGE\n\n"
+  exit 1
+fi
 
 # Use this command to run
-docker run -it --rm -p 8080:8080 --name echo polyverse/tcp_echo_server.$target
+docker run -it --rm -p 8080:8080 --name echo polyverse/tcp_echo_server.$1
 
 # Use this command to debug (and run the server by hand internally, using gdb)
-# docker run --privileged -it --rm -p 8080:8080 --name echo --entrypoint /bin/bash polyverse/tcp_echo_server.$target
+# docker run --privileged -it --rm -p 8080:8080 --name echo --entrypoint /bin/bash polyverse/tcp_echo_server.$1
