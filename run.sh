4ea658330550e3b84f1453c471af570bc400ca34
rsundahl <roy@polyverse.com>
2020-03-03 10:16:01 -0800

    Culled out-of-date targets

--- run.sh
+++ run.sh
@@ -4,20 +4,20 @@ SHORT_DESCRIPTION="Launch the vulnerable tcp-echo-server container for the given
 
 USAGE="$(cat <<EOF
 
-Usage: sh tcp-echo-server.sh ${0##*/} [alpine|centos|trusty|xenial|zesty|artful|bionic]
+Usage: sh $0 [alpine|centos|trusty|xenial|bionic]
 
 $SHORT_DESCRIPTION
 
 EOF
 )"
 
-if [ $# -ne 1 ] || ! [[ "$1" =~ ^(alpine|centos|trusty|xenial|zesty|artful|bionic)$ ]]; then
-  printf "$USAGE\n\n"
-  exit 1
+if [ $# -ne 1 ] || ! [[ "$1" =~ ^(alpine|centos|trusty|xenial|bionic)$ ]]; then
+	printf "$USAGE\n\n"
+	exit 1
 fi
 
 # Use this command to run
-# docker run -it --rm -p 8080:8080 --name echo polyverse/tcp-echo-server.$1
+docker run -it --rm -p 8080:8080 --name echo polyverse/tcp-echo-server.$1
 
 # Use this command to debug (and run the server by hand internally, using gdb)
-docker run --privileged -it --rm -p 8080:8080 -p 8008:8008 --name echo --entrypoint /bin/bash polyverse/tcp-echo-server.$1
+# docker run --privileged -it --rm -p 8080:8080 -p 8008:8008 --name echo --entrypoint /bin/bash polyverse/tcp-echo-server.$1
